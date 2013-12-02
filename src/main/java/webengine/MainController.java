a4a50b3a7031235c558c361ff62e3d2299dd0451
R <r@oo.pl>
2013-12-02 19:07:31 +0100

    search mechanism - basic version

--- src/main/java/webengine/MainController.java
+++ src/main/java/webengine/MainController.java
@@ -2,15 +2,12 @@ package webengine;
 
 import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import returnobjects.Jednostka;
 import sqlqueries.BaseGetMethods;
-import sqlqueries.BaseLoad;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,58 +40,63 @@ public class MainController {
             @RequestParam(value = "phrase", required = true) String phrase) {
 
         Connection conn = Application.connectH2Memory();
-        phrase = "iniczny"; //!!
-        //todo multiple search in the same units
+        //todo mul-iple search in the same units
         //todo limit on fetch rows
         ArrayList<?> colField;
         ArrayList<Integer> matchesResultID = new ArrayList<Integer>();
+        ArrayList<Jednostka> listOfResults = null;
 
         colField = BaseGetMethods.getStrColumnJedn(conn, "Nazwa");
         for (Object s : colField) {
             Pattern pat = Pattern.compile(phrase);
             Matcher m = pat.matcher((String) s);
-            if (m.find() && !matchesResultID.contains(s)) { //one match is enough
+            if (m.find() && !matchesResultID.contains(colField.indexOf(s) + 1)) { //one match is enough
                 matchesResultID.add(colField.indexOf(s) + 1); //array index 0, sql idex = 1
             }
         }
-//        String[] AdrCol = {"Ulica", "Miasto", "NRDOMU", "KODPOCZTOWY", "Dopisek"};
-//        for (int i = 0; i < AdrCol.length; i++) {
-//            //todo validation of telehone, email on client side
-//            colField = BaseGetMethods.getStrColumnAdr(conn, AdrCol[i]);
-//            for (Object s : colField) {
-//                Pattern pat = Pattern.compile(phrase);
-//                Matcher m = pat.matcher((String) s);
-//                if (m.find() && !matchesResultID.contains(s)) {
-//                    matchesResultID.add(colField.indexOf(s) + 1);
-//                }
-//            }
-//        }
 
-//        String[] JednCol = {"Specjalizacja1", "Specjalizacja2", "Specjalizacja3", "Opis", "Www", "Telefon", "Email"};
-//        for (int i = 0; i < JednCol.length; i++) {
-//            //todo validation of telehone, email on client side
-//            colField = BaseGetMethods.getStrColumnJedn(conn, JednCol[i]);
-//            for (Object s : colField) {
-//                Pattern pat = Pattern.compile(phrase);
-//                Matcher m = pat.matcher((String) s);
-//                if (m.find() && !matchesResultID.contains(s)) {
-//                    matchesResultID.add(colField.indexOf(s) + 1);
-//                }
-//            }
-//        }
+        String[] AdrCol = {"Ulica", "Miasto", "NRDOMU", "KODPOCZTOWY", "Dopisek"};
+        for (int i = 0; i < AdrCol.length; i++) {
+            //todo validation of telehone, email on client side
+            colField = BaseGetMethods.getStrColumnAdr(conn, AdrCol[i]);
+            for (Object s : colField) {
+                Pattern pat = Pattern.compile(phrase);
+                Matcher m = pat.matcher((String) s);
+                if (m.find() && !matchesResultID.contains(s)) {
+                    matchesResultID.add(colField.indexOf(s) + 1);
+                }
+            }
+        }
+
+        String[] JednCol = {"Specjalizacja1", "Specjalizacja2", "Specjalizacja3", "Opis", "Www", "Telefon", "Email"};
+        for (int i = 0; i < JednCol.length; i++) {
+            //todo validation of telehone, email on client side
+            colField = BaseGetMethods.getStrColumnJedn(conn, JednCol[i]);
+            for (Object s : colField) {
+                Pattern pat = Pattern.compile(phrase);
+                Matcher m = pat.matcher((String) s);
+                if (m.find() && !matchesResultID.contains(s)) {
+                    matchesResultID.add(colField.indexOf(s) + 1);
+                }
+            }
+        }
 
         String putID = "";
         for (int i = 0; i < matchesResultID.size(); i++) {
             //todo check if '' are required
-            putID += "ID = " + matchesResultID.get(i) + " AND ";
             if (i == (matchesResultID.size() - 1)) {
-                putID += "ID = " + matchesResultID.get(i);
+                putID += "ID=" + matchesResultID.get(i);
+                break;
             }
-            System.out.print(putID);
+            putID += "ID=" + matchesResultID.get(i) + " OR ";
         }
 
-        ArrayList<Jednostka> listOfResults = BaseGetMethods.getAllJednWhere(conn, putID);
+        if( !putID.isEmpty() ){
+            listOfResults = BaseGetMethods.getAllJednWhere(conn, putID);
+        }
+
         Application.closeConnectionH2Memory(conn);
+        //if empty - return nothing
         return listOfResults;
     }
 
